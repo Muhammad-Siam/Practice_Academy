d1f0266652edba1e3ae0160b985c78a2360567a8
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-02-01 15:40:00 +0600

    New uploaded

---
+++ import.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+import "math"
+import "time"
+
+func main() {
+
+	fmt.Printf("I have a new %g problems. \n", math.Sqrt(20))
+	
+	fmt.Println("I have result with %g problems. /n", math.Sqrt(50))
+	
+	fmt.Println(" The time is", time.Now())
+	
+	var name string = "Siam"
+	var age float32 = 16.7
+	
+	fmt.Println(name, age)
+}	
