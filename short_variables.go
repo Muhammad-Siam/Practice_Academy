04df32c14702726c67990b7bd12786aa8803c73d
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-02-05 11:20:02 +0600

    New uploaded

---
+++ short_variables.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+    s := 4
+	r := "siam"
+	y := 'A'
+	fmt.Println(i, j, k, s, r, c, python, java)
+	fmt.Println(string(y))
+}
