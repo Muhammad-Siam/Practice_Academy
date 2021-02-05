258ebf082443ed1f95ef9a2bded289ac26916965
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-02-05 10:56:39 +0600

    New uploaded

---
+++ variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+var s, siam, rahat bool
+
+func main() {
+	var i int=50
+	fmt.Println(i, c, python, java)
+	fmt.Println(s, siam, rahat)
+	}
