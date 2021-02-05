740adf7770b7b960d8db515bf8ba138d47f7528e
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-02-05 11:05:49 +0600

    New uploaded

---
+++ variables2.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, java, golang= true, false, "no!", "yes!"
+	fmt.Println(i, j, c, python, java, golang)
+	var siam, rahat, rupa, sajid = true, false, "no!", "yes!"
+	fmt.Println(siam, rahat, rupa, sajid)
+}
