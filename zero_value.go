0dcd27822fa2608f7860e2c8cd5f9d54d01b1e6a
Muhammad_Siam <[rootersiam2255@gamil.com]>
2021-02-13 22:08:48 +0600

    Zero values in golang

---
+++ zero_value.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var siam string
+	var rahat int
+	var rupa float32
+	var sajid rune
+	var auhid bool
+	var mahad bool
+
+	fmt.Println(siam, rahat, rupa, sajid, auhid)
+}
